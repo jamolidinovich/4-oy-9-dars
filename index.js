f3bd179ba3e0da63933f7ae0828c10dec49e1e10
anazirjonov <anazirjonov10@gmail.com>
2024-01-30 19:32:52 +0500

    masalalar ishlandi

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1-masala
+// function element(arg) {
+//   let arr = [2, 4, 5, 6, 7, 8];
+//   let a = arr.sort(function (a, b) {
+//     return a - b;
+//   });
+//   return a[0];
+// }
+// console.log(element(0));
+
+// 2-masala
+// function yigindiToq(arr) {
+//   let yigindi = 0;
+//   for (let i = 1; i < arr.length; i += 2) {
+//     yigindi += arr[i];
+//   }
+//   return yigindi;
+// }
+// console.log(yigindiToq([1, 2, 3]));
+
+// 3-masala
+//
+
+// 4-masala
+
+// function uchgaQoldiqsizSonlarSoni(massiv) {
+//   let uchgaQoldiqsizSonlarSoni = 0;
+
+//   for (let i = 0; i < massiv.length; i++) {
+//     if (massiv[i] % 3 === 0) {
+//       uchgaQoldiqsizSonlarSoni++;
+//     }
+//   }
+
+//   return uchgaQoldiqsizSonlarSoni;
+// }
+// console.log(uchgaQoldiqsizSonlarSoni([1, 2, 3, 4, 13, 14, 15]));
